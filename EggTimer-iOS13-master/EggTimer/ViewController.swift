8af20e35e86f079d87d6edb4396355518f91eac8
Dahae Kim <iluvdadong@naver.com>
2020-10-14 20:13:22 +0900

    Create hardnessSelected Function

--- EggTimer-iOS13-master/EggTimer/ViewController.swift
+++ EggTimer-iOS13-master/EggTimer/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
     
-
-
+    
+    @IBAction func hardnessSelected(_ sender: UIButton) {
+        
+        print(sender.currentTitle!)
+    }
+    
 }
